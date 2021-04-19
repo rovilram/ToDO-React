6bf123cb7d267f1c7c959aa807fe7386ffafe5e8
rovilram <robertovillaresrama@gmail.com>
2021-04-19 19:48:11 +0200

    finish exercise. Need CSS Styles

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,19 +2,32 @@ import './Form.css';
 import React from 'react';
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
 
-  
+    this.state = {
+      tareas: '',
+    };
+  }
+
   formSubmit = (e) => {
     e.preventDefault();
-    const task = e.target.newTaskText.value;
-    this.props.addNewTask(task);
+    //const task = e.target.newTaskText.value;
+    console.log("ESTADO",this.state)
+    this.props.addNewTask(this.state.tareas);
   };
 
+  handleTitle = (e) => {
+    console.log("INPUT",e.target.value)
+    this.setState(() => ({
+      tareas: e.target.value,
+    }));
+  };
 
   render() {
     return (
       <form onSubmit={(e) => this.formSubmit(e)}>
-        <input type="text" name="newTask" id="newTaskText" />
+        <input type="text" onChange={this.handleTitle} />
         <input type="submit" value="Añadir Tarea" />
       </form>
     );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,14 +8,17 @@ class Main extends React.Component {
     super(props);
 
     this.state = {
-      tareas: [
-        'comprar el pan',
-        'respirar lo máximo posible',
-        'tratar de levantarse a mear de vez en cuando',
-        'sobrevivir',
-        'aprender React',
-      ],
+      tareas: ['a', 'b'],
     };
+  }
+
+  componentDidMount() {
+    this.setState(JSON.parse(localStorage.getItem('state')));
+  }
+
+  componentDidUpdate() {
+    console.log("HAY CAMBIOS")
+    localStorage.setItem('state', JSON.stringify({ tareas: this.state.tareas }));
   }
 
   killTask = (task) => {
@@ -24,28 +27,47 @@ class Main extends React.Component {
     }));
   };
 
-  
   drawTasks = (tasks) => {
-    const tareasArray = tasks.map((task, taskIndex) => (
-      <Task
-      key={task}
-      tarea={{
-        title: task,
-        id: taskIndex,
-      }}
-      clickFunction={this.killTask}
-      />
+    let tareasArray = [];
+    console.log('TAREAS', tasks);
+    if (tasks.length !== 0) {
+      tareasArray = tasks.map((task, taskIndex) => (
+        <Task
+          key={task}
+          tarea={{
+            title: task,
+            id: task,
+          }}
+          killTask={this.killTask}
+          saveTask={this.saveTask}
+        />
       ));
-      return tareasArray;
-    };
-    
-    addNewTask = (task) => {
-      this.setState((prevState) => ({
-        tareas: [...prevState.tareas, task]
-      }))
     }
-    
-    render() {
+    return tareasArray;
+  };
+
+  addNewTask = (task) => {
+    this.setState((prevState) => ({
+      tareas: [...prevState.tareas, task],
+    }));
+  };
+
+  saveTask = (task) => {
+    this.setState((prevState) => {
+      const newState = prevState.tareas.map((tarea) => {
+        if (tarea === task.id) {
+          console.log(`MODIFICADO ${task.id}`);
+          return task.tarea;
+        } else {
+          return tarea;
+        }
+      });
+      console.log('NUEVO ESTADO', newState);
+      localStorage.setItem('state', JSON.stringify({ tareas: newState }));
+      return newState;
+    });
+  };
+  render() {
     return (
       <main>
         <Form addNewTask={this.addNewTask} />
